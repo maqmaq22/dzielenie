8774411f0e94e2c5e39db69002c07a0fe966af7f
Tomek <madmak2005@gmailcom>
2022-01-29 19:45:45 +0100

    format kodu

--- dzielenie/dzielenie.cpp
+++ dzielenie/dzielenie.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main() {
 	string li1, li2, dl;
-	string napis2,napis1;
+	string napis2, napis1;
 	//cout << "jaka jesli 1 liczba?";
 	//cin >> li1;
 	//cout << "jaka jesli 2 liczba?";
@@ -18,21 +18,21 @@ int main() {
 	int ileCyfr1 = napis1.length();
 	int ileCyfr2 = napis2.length();
 	int x = 1;
-	int liczba1=0;
+	int liczba1 = 0;
 	bool pierwszy_raz = true;
 	for (int i = ileCyfr2 - 1; i >= 0; i--) {
 		int cyfra1 = napis1[i] - '0';
 
-			liczba1 = liczba1 + cyfra1 * x;
-			x = x * 10;
-		
+		liczba1 = liczba1 + cyfra1 * x;
+		x = x * 10;
+
 		cout << liczba1 << endl;
 	}
-	
+
 	if (liczba1 >= dziel) {
-		int wyn=liczba1 - (liczba1 / dziel)*dziel;
+		int wyn = liczba1 - (liczba1 / dziel)*dziel;
 		cout << "Wynik: ";
-	cout << wyn << endl;
+		cout << wyn << endl;
 	}
 	else {
 		liczba1 = 0;
@@ -40,8 +40,8 @@ int main() {
 		int x = 1;
 		for (int i = ileCyfr2; i >= 0; i--) {
 			int cyfra1 = napis1[i] - '0';
-				liczba1 = liczba1 + cyfra1 * x;
-				x = x * 10;
+			liczba1 = liczba1 + cyfra1 * x;
+			x = x * 10;
 			cout << liczba1 << endl;
 		}
 		int wyn = liczba1 - (liczba1 / dziel)*dziel;
